5dd3533dd794e57cf44113933d9a6f21a1efc1b7
George Kour <george.kour@hp.com>
2016-11-30 18:02:02 +0200

    Test our implementation in an SCA-HMM environment and compare it to HMM performace. compare: 1. posterior probability, 2. hit rate of the states

--- test_decode_mazehmmvshmm.m
+++ test_decode_mazehmmvshmm.m
@@ -1,23 +1,24 @@
-function test_viterbimazehmmvshmm()
+function test_decode_mazehmmvshmm()
+% generate a sequence of trials using mazehmmgenerate with two environment type but no rewarded states.
+% The estimated transition and emmits probabilities should be the same as the sequence generation parameters.
+% The guess in this test is the same as the equence generation parametrres.
+
 res_hmmprobs = [];
 res_mazehmmprobs = [];
 res_mazehmmprobsreal = [];
-for i=1:5
+for i=1:2
     [hmmprobs, mazehmmprobs, mazehmmprobsreal]=calcseqposteriorprobability(100,1000,5);
     res_hmmprobs = [res_hmmprobs; hmmprobs];
     res_mazehmmprobs = [res_mazehmmprobs; mazehmmprobs];
     res_mazehmmprobsreal=[res_mazehmmprobsreal;mazehmmprobsreal];
 end
 
-norm_mazehmmprobs = res_mazehmmprobs./res_mazehmmprobsreal.*100;
-norm_hmmprobs = res_hmmprobs./res_mazehmmprobsreal.*100;
+norm_mazehmmprobs = res_mazehmmprobs-res_mazehmmprobsreal;
+norm_hmmprobs = res_hmmprobs-res_mazehmmprobsreal;
 
 
-bar(floor(linspace(100,1000,5))', [mean(norm_mazehmmprobs); mean(norm_hmmprobs)]','LineWidth',1.5)
-legend('MBW', 'BW')
-xlabel('Sequence length')
-ylabel('Normalized percentage of correct states estimation')
-title('The normalized percentage of correct states estimation using on a sequence of 200 trials')
+bar(floor(linspace(100,1000,5))', [mean(res_mazehmmprobsreal); mean(res_mazehmmprobs); mean(res_hmmprobs)]','LineWidth',1.5)
+legend('sca-hmm real','sca-hmm', 'hmm')
 
 end
 
@@ -57,11 +58,11 @@ function [hmmprobs, mazehmmprobs, mazehmmprobsreal] = calcseqposteriorprobabilit
 env_type_frac = 0.5;
 [envtype,emissions, states, rewards] = ...
     mazehmmgenerate(1500, realTRr, realTRnr, ...
-    realEhomo, realEhetro ,env_type_frac, [1 0; 1 0]);
+    realEhomo, realEhetro ,env_type_frac, [1 0; 0 1]);
 
 [testseq.envtype,testseq.emissions, testseq.states, testseq.rewards] = ...
     mazehmmgenerate(200, realTRr, realTRnr, ...
-    realEhomo, realEhetro ,env_type_frac, [1 0; 1 0]);
+    realEhomo, realEhetro ,env_type_frac, [1 0; 0 1]);
 
 
 guess.tr = getrandomdistribution(4,4);
@@ -80,35 +81,32 @@ for seq_length=floor(lengths)
     seq_data.emissions = emissions(1:seq_length);
     seq_data.rewards = rewards(1:seq_length);
     
-    [mazehmmestimate,hmmestimate] = train_models(seq_data, guess, max_iter);
+    [mazehmmestimate,hmmestimate] = run_train(seq_data, guess, max_iter);
     
-    [correctstates_mazehmm,correctstates_hmm ,correctstates_mazehmmreal] = statesHammingDistance(mazehmmestimate,hmmestimate, testseq);
+    postprobs = decode(mazehmmestimate,hmmestimate, testseq);
     
-    mazehmmprobs = [mazehmmprobs, correctstates_mazehmm];
-    hmmprobs = [hmmprobs, correctstates_hmm];
-    mazehmmprobsreal= [mazehmmprobsreal, correctstates_mazehmmreal];
+    mazehmmprobs = [mazehmmprobs, postprobs(1)];
+    hmmprobs = [hmmprobs, postprobs(2)];
+    mazehmmprobsreal= [mazehmmprobsreal, postprobs(3)];
     
 end
 end
 
-function [correctstates_mazehmm,correctstates_hmm ,correctstates_mazehmmreal] = statesHammingDistance(mazehmmestimate,hmmestimate,testseq)
+function postprobs = decode(mazehmmestimate,hmmestimate,testseq)
 
-estimatedstates_mazehmm = mazehmmviterbi(testseq.emissions,testseq.envtype,testseq.rewards,...
+[~,pSeqmazehmm, ~, ~, ~] = mazehmmdecode(testseq.emissions,testseq.envtype,testseq.rewards,...
     mazehmmestimate.tr_reward,mazehmmestimate.tr_noreward,mazehmmestimate.e_homo,mazehmmestimate.e_hetro);
 
-estimatedstates_hmm = hmmviterbi(testseq.emissions,hmmestimate.tr,hmmestimate.e);
+[~,pSeqhmm, ~, ~, ~] = hmmdecode(testseq.emissions,hmmestimate.tr,hmmestimate.e);
 
 [realTRr, realTRnr, realEhomo, realEhetro] = get_real_parameters();
-estimatedstates_real=mazehmmviterbi(testseq.emissions,testseq.envtype,testseq.rewards,realTRr,realTRnr,realEhomo,realEhetro);
+[~,pSeqmazehmmReal, ~, ~, ~]=mazehmmdecode(testseq.emissions,testseq.envtype,testseq.rewards,realTRr,realTRnr,realEhomo,realEhetro);
 
-correctstates_mazehmm = sum(estimatedstates_mazehmm==testseq.states)/200;
-correctstates_hmm = sum(estimatedstates_hmm==testseq.states)/200;
-correctstates_mazehmmreal = sum(estimatedstates_real==testseq.states)/200;
-
+postprobs = [pSeqmazehmm, pSeqhmm, pSeqmazehmmReal];
 
 end
 
-function [mazehmmestimate,hmmestimate] = train_models(seq_data, guess, max_iter) 
+function [mazehmmestimate,hmmestimate] = run_train(seq_data, guess, max_iter) 
 
 [mazehmmestimate.tr_reward, mazehmmestimate.tr_noreward, mazehmmestimate.e_homo, mazehmmestimate.e_hetro] = ...
     mazehmmtrain(seq_data.emissions, seq_data.envtype , seq_data.rewards ,guess.tr ,guess.tr ,...
@@ -119,3 +117,5 @@ function [mazehmmestimate,hmmestimate] = train_models(seq_data, guess, max_iter)
     hmmtrain(seq_data.emissions, guess.tr, guess.tr, 'VERBOSE',false, 'maxiterations', max_iter);
 
 end
+
+
