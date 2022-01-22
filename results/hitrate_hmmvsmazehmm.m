2ef1a7d65aed9001c192e1b0a2661c544fac0a47
George Kour <kourgeorge@gmail.com>
2022-01-22 22:45:11 +0200

    rename hotrate funtion.

--- results/hitrate_hmmvsmazehmm.m
+++ results/hitrate_hmmvsmazehmm.m
@@ -1,4 +1,4 @@
-function pdiv_hmmvsmazehmm()
+function hitrate_hmmvsmazehmm()
 
 training_seq_lengths = floor(linspace(50,2000,50));
 repetitions = 25;
@@ -112,20 +112,6 @@ end
 end
 
 
-function total_policy_dist_sequence = calcActionProbDivergence(estimated_policies_seq, testseq, estimated_theta, true_theta)
-environment_states = testseq.envtype;
-true_policies_seq = testseq.states;
-true_policies = CreatePolicies([{true_theta.eH},{true_theta.eT}]);
-estimated_policies = CreatePolicies([{estimated_theta.eH}, {estimated_theta.eT}]);
-total_policy_dist_sequence = 0;
-sequence_length = length(environment_states);
-for t=1:sequence_length
-    current_state = environment_states(t);
-    current_true_policy = true_policies{true_policies_seq(t)};
-    current_estimated_policy = estimated_policies{estimated_policies_seq(t)};
-    total_policy_dist_sequence=total_policy_dist_sequence+JSDiv(current_true_policy(current_state,:),current_estimated_policy(current_state,:) );
-end
-end
 
 function gramm_graph(hitrates, models, training_seq_lengths)
 
