8a48231c258d96974ce2458bd6f4ca5bdd1aed3e
George Kour <kourgeorge@gmail.com>
2022-08-12 11:13:36 +0300

    compare models parameters.

---
+++ results/metrics/paramatersJS.m
@@ -0,0 +1,16 @@
+function [trans_JS,policies_JS] = paramatersJS (theta_gt,theta_hat)
+%PARAMATERSJS Summary of this function goes here
+%   Detailed explanation goes here
+
+[match, match_distances, policies_dist] = MatchandComparePolicies([{theta_gt.eH},{theta_gt.eT}],[{theta_hat.eH}, {theta_hat.eT}]);
+match= match(:,1);
+est_trans_noreward = theta_hat.trNR(match,:);
+est_trans_noreward = est_trans_noreward(:,match);
+est_trans_reward = theta_hat.trR(match,:);
+est_trans_reward = est_trans_reward(:,match);
+
+trans_JS = mean([mean(JSDiv(est_trans_reward ,theta_gt.trR)), mean(JSDiv(est_trans_noreward ,theta_gt.trNR))]);
+policies_JS = mean(match_distances);
+
+end
+
