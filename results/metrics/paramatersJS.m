3407e0c964232f8557a2a8a508c90e3f99b1f5d8
George Kour <kourgeorge@gmail.com>
2022-08-13 19:00:20 +0300

    adapt to neural policices.

--- results/metrics/paramatersJS.m
+++ results/metrics/paramatersJS.m
@@ -2,7 +2,12 @@ function [trans_JS,policies_JS] = paramatersJS (theta_gt,theta_hat)
 %PARAMATERSJS Summary of this function goes here
 %   Detailed explanation goes here
 
-[match, match_distances, policies_dist] = MatchandComparePolicies([{theta_gt.eH},{theta_gt.eT}],[{theta_hat.eH}, {theta_hat.eT}]);
+if isfield(theta_gt, 'eH')
+    [match, match_distances, policies_dist] = MatchandComparePolicies([{theta_gt.eH},{theta_gt.eT}],[{theta_hat.eH}, {theta_hat.eT}]);
+else
+    [match,match_distances,policies_dist] = MatchGamblingPolicies(theta_gt.policies, theta_hat.policies);
+end
+
 match= match(:,1);
 est_trans_noreward = theta_hat.trNR(match,:);
 est_trans_noreward = est_trans_noreward(:,match);
