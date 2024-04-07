41d1f85da93df50268c9b508c52bd2612664c664
George Kour <kourgeorge@gmail.com>
2024-04-08 00:40:59 +0300

    chnage terminology

--- results/gamblimg/main.m
+++ results/gamblimg/main.m
@@ -1,35 +1,75 @@
+close all;
 p1 = LinearPolicy(1,0);
-p2 = LinearPolicy(0.5,-0.2);
-trR = [0.9, 0.1; 0.9, 0.1];
-trNR = [0.1, 0.9; 0.1, 0.9];
-theta_gt.trR = trR;
-theta_gt.trNR = trNR;
-theta_gt.policies = {p1, p2};
+p2 = AntiLinearPolicy(1,0);
+% % 
+% p1 = BandPolicy(1,0,0.3);
+% p2 = BandPolicy(-1,0.3,0.6);
+
+
+theta_gt= get_neural_gt_parameters();
+%theta_gt.policies = {p1, p2};
+
+
 
 agent = GamblingAgent(theta_gt.trR , theta_gt.trNR, theta_gt.policies);
-interaction = GamblingGame(60);
+interaction = GamblingGame(1000);
 interaction.start(agent)
+%interaction.draw_empirical_decision_boundary()
 
-[interaction.hidden_state;interaction.actions;interaction.wins]
+[interaction.hidden_states;interaction.actions;interaction.wins]
 
+%create a network mimiking the band policy
+ %mimicking_network = NetworksToNeuralPolicies(MimicGamblingPolicy({p1}, 100));
+ %CompareGamblingPolicies(mimicking_network{1} ,theta_gt.policies{1}, 500);
+ 
+ 
+guess = get_neural_random_gt_parameters();
+guess.policies = RandomNetworks(2);
 
-guess_trans_reward = [0.6 0.4; 0.4 0.6];
-guess_trans_noreward = [0.8 0.2; 0.2 0.8];
-
-networks = RandomNetworks(2);
 
 [theta_hat.trR, theta_hat.trNR, theta_hat.policies] = ...
     scahmmtrain(interaction.actions, num2cell(interaction.states, 2)' , interaction.wins ...
-            ,guess_trans_reward ,guess_trans_noreward , networks);
+            ,guess.trR ,guess.trNR , guess.policies);
+
+% compare the policies in each set of parameters.
+distance_gts = CompareGamblingPolicies(theta_gt.policies{1} ,theta_gt.policies{2}, 500);
+
+guess_policies = NetworksToNeuralPolicies(guess.policies);
+distance_guesses = CompareGamblingPolicies(guess_policies{1} ,guess_policies{2}, 500);
+
+hat_policies = NetworksToNeuralPolicies(theta_hat.policies);
+distance_estimated = CompareGamblingPolicies(hat_policies{1} ,hat_policies{2}, 500);
 
 
-estimated_policies_seq = scahmmviterbi(interaction.actions, num2cell(interaction.states,2), interaction.rewards,...
+[gues_match,guess_match_distances,guess_policies_dist] = ...
+    MatchGamblingPolicies(theta_gt.policies, NetworksToNeuralPolicies(guess.policies));
+[match,match_distances,policies_dist] = ...
+    MatchGamblingPolicies(theta_gt.policies,NetworksToNeuralPolicies(theta_hat.policies));
+
+sum(guess_match_distances)
+sum(match_distances)
+
+
+estimatedstatesNBIRD = scahmmviterbi(interaction.actions, num2cell(interaction.states,2), interaction.rewards,...
     theta_hat.trR, theta_hat.trNR, theta_hat.policies);
 
+estimatedstatesNBIRDGuess = scahmmviterbi(interaction.actions, num2cell(interaction.states,2), interaction.rewards,...
+    guess.trR, guess.trNR, guess.policies);
 
-%state_hit_rate = mean(estimated_policies_seq==interaction.hidden_state);
-%action_hit_rate = mean(policies{estimated_policies_seq}.act()==interaction.actions);
+policy_hit_rate = MatchingCBIRDPoliciesHitrate(estimatedstatesNBIRD, interaction.hidden_states, theta_hat, theta_gt);
 
-%accuracy = compare_preference_curve(p1 ,NeuralPolicy(models_hat{2}), 100);
 
-[match,match_distances,policies_dist] = MatchGamblingPolicies(theta_gt.policies,NetworksToNeuralPolicies(theta_hat.policies));
+%Pdiv
+% 
+% table_policies = tabulate_neural_policies(theta_hat.policies, 2);
+% theta_hat.eH = table_policies{1};
+% theta_hat.eT = table_policies{2};
+% 
+% table_policies = tabulate_neural_policies(theta_gt.policies, 2);
+% theta_gt.eH = table_policies{1};
+% theta_gt.eT = table_policies{2};
+%     
+%     
+% NBIRDPdiv = mean(PolicyDivergence(estimatedstatesNBIRD, interaction.hidden_states, theta_hat, theta_gt));
+% NBIRDPdivGuess = mean(PolicyDivergence(estimatedstatesNBIRDGuess, interaction.hidden_states, guess, theta_gt));
+% 
